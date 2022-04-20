21f0663cbdc949449da3327f140cf5b1e926513b
KMBarber1 <katherine.m.barber@outlook.com>
2022-04-20 00:55:09 -0700

    initial submit

---
+++ src/main/java/com/katebarber/daikichipathvariables/DaikichiPathVariableApplication.java
@@ -0,0 +1,16 @@
+package com.katebarber.daikichipathvariables;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DaikichiPathVariableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaikichiPathVariableApplication.class, args);
+		
+	}
+	
+}
---
+++ src/main/java/com/katebarber/daikichipathvariables/DaikichiPathVariableController.java
@@ -0,0 +1,43 @@
+package com.katebarber.daikichipathvariables;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping
+public class DaikichiPathVariableController {
+	
+	@RequestMapping("/")
+	public String welcome() {
+		return "Welcome!";
+	}
+	
+	@RequestMapping("/today")
+	public String today() {
+		return "Today you will find luck in all your endeavors!";
+	}
+	
+	@RequestMapping("/tomorrow")
+	public String tomorrow() {
+		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+	
+	@RequestMapping("/travel/{place}")
+	public String travel_city(@PathVariable("city") String city) {
+		return "Congratulations! You will soon travel to " + city + "!";
+	}
+	
+	@RequestMapping("/lotto/{number}")
+	public String lotto(@PathVariable("number") int number) {
+		if(number % 2 == 0) {
+			return "You will take a grand journey in the near future, but be wary of tempting offers.";
+		} else {
+			return "You have enjoyed the fruits of you labor but now is a great time to spend time with family and friends.";
+		}
+		
+	}
+
+
+}
